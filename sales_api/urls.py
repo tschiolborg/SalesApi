86b0365c087564347ab3b8646bcdc68650b4e963
Tobias Schiolborg <tschiolborg@gmail.com>
2022-03-25 22:40:37 +0100

    basic form

--- sales_api/urls.py
+++ sales_api/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("create_new_product/", views.create_new_product, name="create_new_product"),
+    path("delete_product/<int:id>/", views.delete_product, name="delete_product"),
 ]
--- sales_api/views.py
+++ sales_api/views.py
@@ -1,5 +1,7 @@
+from decimal import Decimal
+
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 from .models import Product
 
@@ -8,5 +10,34 @@ def index(request):
     all_products = Product.objects.all()
     context = {
         "all_products": all_products,
+        "active": "products",
     }
     return render(request, "sales_api/index.html", context)
+
+
+def create_new_product(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        price = request.POST.get("price")
+        if name is not None and name != "":
+            try:
+                price = Decimal(price)
+            except:
+                return HttpResponse("Error: Cannot read price")
+
+            Product.objects.create(
+                name=name,
+                price=price,
+            )
+            return redirect("/sales_api/")
+        else:
+            return HttpResponse("Error: No name given")
+    return HttpResponse("Error: no post")
+
+
+def delete_product(request, id):
+    try:
+        Product.objects.filter(id=id).delete()
+    except:
+        return HttpResponse(f"Error: Could not delete.")
+    return redirect("/sales_api/")
