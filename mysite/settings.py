7ac9bc4ccfcaa2b6751f32b556b99476940d5b05
Tobias Schiolborg <tschiolborg@gmail.com>
2022-03-25 19:23:33 +0100

    basic js

--- mysite/settings.py
+++ mysite/settings.py
@@ -29,8 +29,8 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 # Media directory
-MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
 
 
 # Application definition
@@ -123,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
