06c8990ece010eca3e525ae398975d0c66cda7f4
Tobias Schiolborg <tschiolborg@gmail.com>
2022-03-26 18:21:52 +0100

    update additions

--- sales_api/static/sales_api/additions.js
+++ sales_api/static/sales_api/additions.js
@@ -1,44 +1,49 @@
-var modal = document.getElementById("add_modal");
-var inner_modal_text = document.getElementById("add_selected_item");
-var items = document.getElementsByClassName("add_open_modal");
-var close_btn = document.getElementsByClassName("close")[0];
-var increase_btn = document.getElementById("add_increase");
-var decrease_btn = document.getElementById("add_decrease");
-var input_field = document.getElementById("add_count_input");
-var add_form = document.getElementById("add_form");
-var selected_item = null;
+import { select, inc_dec_input } from './utils.js';
 
-// onclick for each item: open modal
-for (var i = 0; i < items.length; i++) {
-    items[i].onclick = function () {
-        select(this);
+set_up_add();
+
+
+function set_up_add() {
+
+    var modal = document.getElementById("add_modal");
+    var inner_modal_text = document.getElementById("add_selected_item");
+    var items = document.getElementsByClassName("add_open_modal");
+    var close_btn = document.getElementsByClassName("close")[0];
+    var increase_btn = document.getElementById("add_increase");
+    var decrease_btn = document.getElementById("add_decrease");
+    var input_field = document.getElementById("add_count_input");
+    var form = document.getElementById("add_form");
+
+    // onclick for each item: open modal
+    for (var i = 0; i < items.length; i++) {
+        items[i].onclick = function () {
+            set_select(this)
+        }
+    }
+
+    // close modal
+    close_btn.onclick = function () {
+        set_select(null)
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            set_select(null)
+        }
+    }
+
+    // set increase / decrease btns
+    increase_btn.onclick = function () { inc_dec_input(true, input_field); }
+    decrease_btn.onclick = function () { inc_dec_input(false, input_field); }
+
+    function set_select(item) {
+        select(item, modal, inner_modal_text);
+        form.action = item == null ? "/sales_api/add_count/" : "/sales_api/add_count/" + item.id.slice(4) + "/";
+        input_field.value = 0;
     }
 }
 
-// close modal
-close_btn.onclick = function () { select(null); }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-    if (event.target == modal) {
-        select(null);
-    }
-}
 
-// set increase / decrease btns
-increase_btn.onclick = function () { inc_dec_input(true); }
-decrease_btn.onclick = function () { inc_dec_input(false); }
 
-// set selected item
-function select(item) {
-    selected_item = item;
-    modal.style.display = item == null ? "none" : "block";
-    inner_modal_text.innerHTML = item == null ? "" : "Add " + selected_item.innerHTML;
-    add_form.action += item == null ? "" : item.id.slice(4) + "/";
-}
-
-// increase or decrease input field
-function inc_dec_input(inc) {
-    var v = parseInt(input_field.value);
-    input_field.value = inc ? v + 1 : Math.max(v - 1, 0);
-}
---
+++ sales_api/static/sales_api/utils.js
@@ -0,0 +1,13 @@
+
+// set selected item
+export function select(item, modal, inner_modal_text) {
+    modal.style.display = item == null ? "none" : "block";
+    inner_modal_text.innerHTML = item == null ? "" : "Add " + item.innerHTML;
+}
+
+
+// increase or decrease input field
+export function inc_dec_input(inc, input_field) {
+    var v = parseInt(input_field.value);
+    input_field.value = inc ? v + 1 : Math.max(v - 1, 0);
+}
