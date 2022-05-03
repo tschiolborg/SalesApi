35d77b05f800e09d0e4490f28886e80219c7a3a8
Tobias Schiolborg <tschiolborg@gmail.com>
2022-05-03 19:16:56 +0200

    removed unnecessary field

--- sales_api/models.py
+++ sales_api/models.py
@@ -2,11 +2,12 @@ from django.db import models
 
 
 class Product(models.Model):
-    id = models.AutoField(primary_key=True)
+    """A product that we are selling"""
+
     name = models.CharField(max_length=256, unique=True)
     count = models.PositiveIntegerField(default=0)
     image = models.ImageField(upload_to="images/", default="images/no_photo.jpeg")
-    price = models.DecimalField(decimal_places=2, max_digits=12)
+    price = models.DecimalField(decimal_places=2, max_digits=12)  # add min value
 
     def increase_count(self, amount):
         self.count += amount
@@ -26,8 +27,11 @@ class Product(models.Model):
 
 
 class Transaction(models.Model):
-    id = models.AutoField(primary_key=True)
+    """A transaction of a product"""
+
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True, default=None)
     amount = models.PositiveIntegerField(default=0)
     total_price = models.DecimalField(decimal_places=2, max_digits=12)
     date = models.DateTimeField("date of transaction", auto_now_add=True)
+
+    # add user, who did the transaction
